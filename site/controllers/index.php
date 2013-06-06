14707500b5af067bed03007f5c22a59dcae089f1
EvgeniiUA <sinivid-jenya@mail.ru>
2013-06-06 07:11:28 +0300

    Update index.php

--- site/controllers/index.php
+++ site/controllers/index.php
@@ -2,8 +2,16 @@
 
 namespace site\controllers;
 
+/**
+ * Default controller
+ * 
+ * @author Sinevid Evgenii (sinevid-evgenii@mail.ru)
+ */
 class index extends \framework\Controller {
-
+    
+    /**
+     * default action
+     */
     public function actionIndex() {
         $this->render('index');
     }
